6ede69b6965a54f5fefef0c75ee77cf3c5bc5bc4
stachu3478 <stachu3478@o2.pl>
2021-04-11 14:46:22 +0200

    Use multiplying for internal iteration check

--- projekt_omp/BasePrimes.cpp
+++ projekt_omp/BasePrimes.cpp
@@ -3,18 +3,25 @@
 BasePrimes::BasePrimes(int maxValue) {
     maxPrimesCount = sqrt(maxValue);
     primes = new int[maxPrimesCount];
+    primesSquares = new int[maxPrimesCount];
 };
+
+BasePrimes::~BasePrimes() {
+    delete[] primes;
+    delete[] primesSquares;
+}
 
 void BasePrimes::getPrimesToCheck() {
     for (int d = 2; d <= maxPrimesCount; d++) {
         if (isPrime(d)) {
-            primes[primesCount++] = d;
+            primes[primesCount] = d;
+            primesSquares[primesCount++] = d * d;
         }
     }
 }
 
 bool BasePrimes::isPrime(int number) {
-    for (int i = 0; i < primesCount && primes[i] <= sqrt(number); i++) {
+    for (int i = 0; i < primesCount && primesSquares[i] <= number; i++) {
         if (number % primes[i] == 0) {
             return false;
         }
--- projekt_omp/BasePrimes.h
+++ projekt_omp/BasePrimes.h
@@ -6,10 +6,12 @@ class BasePrimes
 {
 public:
 	BasePrimes(int maxValue);
+	~BasePrimes();
 	void getPrimesToCheck();
     bool isPrime(int number);
 private:
 	int* primes;
+	int* primesSquares;
 	int primesCount = 0;
 	int maxPrimesCount;
 };
